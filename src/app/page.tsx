3c071d70b99aebf59e623739e35f3ff5beed6d92
Joe Rupslauskas <joerupslauskas@Joes-MacBook-Pro-2.local>
2025-10-08 19:13:01 -0500

    Add data source credit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <p>Astronaut based learning with React and Next.js</p>
-      <p>Home Page</p>
+      <p>Data in this app powered by the awesome efforts over at <a href="http://open-notify.org" target="_blank" rel="noopener noreferrer">Open Notify</a></p>
     </main>
   )
 }
